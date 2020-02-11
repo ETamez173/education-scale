cf0dfc1a43de89583dba58e8b21640b2ee14f995
ETamez9173 <elitamez17@gmail.com>
2020-02-11 17:31:24 -0600

    finished school name on School Search and My School Option - used expand on fetch

--- src/components/degreeschool/DegreeSchool.js
+++ src/components/degreeschool/DegreeSchool.js
@@ -1,49 +1,141 @@
-import React from "react"
+// import React from "react"
+import React, { useContext } from "react"
 import "./DegreeSchool.css"
 import { DegreeContext } from "../degree/DegreeProvider";
 import { SchoolContext } from "../school/SchoolProvider"
+import { LoanContext } from "../loan/LoanProvider"
 
 import School from "../school/School"
+import Loan from "../loan/Loan"
 
 ////aka SchoolList.js   
 
-export default ({ degreeSchool, loans, degree, school }) => (
 
-    
-    <section className="degreeSchool__section">
+// export default ({ degreeSchool, loans, degree, school }) => (
+export default ({ degreeSchool, loan, degree, school }) => {
 
-        <h3 className="degreeSchool__name"></h3>
 
-        <section className="tableMyCBA">
+    const { addLoan } = useContext(LoanContext)
 
-            <div className="degreeSchool__nameCareer">{degreeSchool.degree.educationName}</div>
-           
-           {/* Need to get the School Name here.  Maybe a find on school object   */}
-           
-            <div className="place__holder">
-               { 
-                // school.find(school.id === degreeSchool.schoolId)
-               }
-                PLACE HOLDER</div>
 
-           <div className="degreeSchool__nameCareer">${degreeSchool.annualCost}</div> 
-            <div className="degreeSchool__nameCareer">${degreeSchool.totalCost}</div> 
-            {/* <div className="degreeSchool__totalCost">${degreeschool.degree.totalCost}</div> */}
-           
+    const constructNewSchoolOption = () => {
 
-            <div className="finWorkBench__buttons">
-                <div>
-                    <button className="saveSchoolListItem__button">Save </button>
-         
-                    <button className="deleteSchoolListItem__button">Delete</button>
-                </div>
-                <div>
+        //     /// Creating the object right here >>>>>
+
+        const ActiveUser = localStorage.getItem("education_customer")
+        // const id =                      ;
+        // const educationName = degree.educationName;
+        // const earningsAvg = degree.earningsAvg;
+        // const searchSchool = "False";
+        // const userId = parseInt(localStorage.getItem("educationScale_user"));
+
+        const userId = null;
+        const educationName = null;
+        const schoolName = null;
+        const annualSchoolCost = null;
+        const schoolTotalCost = null;
+        //  these items below are blank until used later
+        const loanAmount = null;
+        const loanRate = null;
+        const loanLengthMonths = null;
+        const loanPmt = null;
+        const totalLoanPmts = null;
+        const cashPaid = null;
+        const totalAmountPaid = null;
+        const degreeAnnualEarnings = null;
+        const twentyYearEarnings = null;
+        const benefitCostRatio = null
+
+
+        addLoan({
+
+
+            userId: parseInt(ActiveUser),
+            educationName: degreeSchool.degree.educationName,
+            schoolName: degreeSchool.school.schoolName,
+            annualSchoolCost: degreeSchool.annualCost,
+            schoolTotalCost: degreeSchool.totalCost,
+            //  these items below are blank until used later
+            loanAmount: "",
+            loanRate: "",
+            loanLengthMonths: "",
+            loanPmt: "",
+            totalLoanPmts: "",
+            cashPaid: "",
+            totalAmountPaid: "",
+            degreeAnnualEarnings: "",
+            twentyYearEarnings: "",
+            benefitCostRatio: ""
+
+            // educationName: degree.educationName,
+            // searchSchool: "False",
+            // earningsAvg: degree.earningsAvg,
+            // userId: parseInt(ActiveUser),
+            // degreeId: degree.id
+
+        })
+
+        //     // .then(() => props.history.push("/degrees"));
+
+
+    }
+
+    return (
+
+        <section className="degreeSchool__section">
+
+            <h3 className="degreeSchool__name"></h3>
+
+            <section className="tableMyCBA">
+
+                
+                {/* <div className="degreeSchool__nameCareer">{degreeSchool.degree.educationName}</div> */}
+                {/* used a fetch with 2 expands  */}
+                {/* return fetch("http://localhost:8088/degreeSchools?_expand=degree&_expand=school") */}
+                
+                                <div className="degreeSchool__nameCareer">{degreeSchool.degree.educationName}</div>
+                                <div className="degreeSchool__nameSchool">{degreeSchool.school.schoolName}</div>
+
+                                <div className="degreeSchool__nameCareer">${degreeSchool.annualCost}</div>
+                                <div className="degreeSchool__nameCareer">${degreeSchool.totalCost}</div>
+                {/* <div className="degreeSchool__totalCost">${degreeschool.degree.totalCost}</div> */}
+
+
+                <div className="finWorkBench__buttons">
+                    <div>
+                        <button className="saveSchoolListItem__button">Save </button>
+
+                        <button className="deleteSchoolListItem__button">Delete</button>
+                    </div>
+
+                    {/* use this button to create the loan object that will be used fore rest of analysis */}
+                    {/* <div>
                     <button className="moveToBenefitAnalysis__button">Add to School Shortlist</button>
+                </div> */}
+
+
+                    <button className="addToSchoolOptionShortlist_button"
+                        onClick={() => {
+
+                            constructNewSchoolOption()
+                            // this create the loan object
+
+                            // see code for degree
+
+                        }}
+                    >
+                        Add to School Shortlist
+                </button>
+
+
+
+
                 </div>
-            </div>
+            </section>
+
+
         </section>
 
+    );
 
-    </section>
-
-)
+}
